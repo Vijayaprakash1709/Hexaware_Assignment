5c666bd4f691b55d803120a9c1080080f2caf250
vijayaprakash1709 <vijayaprakash885@gmail.com>
2024-11-13 09:34:49 +0530

    C# file added to Git

---
+++ Courier_Management_System/Courier_Management_System/Courier_Management_System C#/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Courier_Management_System_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s=Console.ReadLine();
+            if (s == "Processing")
+            {
+                Console.WriteLine("Processing");
+            }
+            else if(s=="Delivered")
+            {
+                Console.WriteLine("Delivered");
+            }
+            else if (s == "Cancelled")
+            {
+                Console.WriteLine("Cancellled");
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
